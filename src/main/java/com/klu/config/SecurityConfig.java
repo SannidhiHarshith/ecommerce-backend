d29ff6ed4fe03ae3610bd7011fc693aefb0f6591
Harshith kumar Sannidhi <2400032943@kluniversity.in>
2026-03-28 07:44:25 +0530

    Update SecurityConfig.java

--- src/main/java/com/klu/config/SecurityConfig.java
+++ src/main/java/com/klu/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-            "ecommerce-frontend-production-914d.up.railway.app"
+            "https://ecommerce-frontend-production-914d.up.railway.app"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
